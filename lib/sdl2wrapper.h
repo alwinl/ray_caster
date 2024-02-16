975a69a241b8a31d149adf50487fa88e649fa676
Alwin Leerling <dna.leerling@gmail.com>
2024-02-17 10:14:21 +1300

    Imported old ray caster project.

---
+++ lib/sdl2wrapper.h
@@ -0,0 +1,115 @@
+/*
+ * sdl2wrapper.h Copyright 2024 Alwin Leerling dna.leerling@gmail.com
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ */
+
+#include <string>
+
+#include <SDL2/SDL.h>
+
+struct SetupParams {
+	std::string title;
+	int width;
+	int height;
+	uint32_t flags; // potential to make this an enum class
+	int rendererFlags = SDL_RENDERER_ACCELERATED;
+};
+
+class Game
+{
+public:
+	Game() = default;
+	virtual ~Game() = default;
+
+	SetupParams make_setup() { return get_params(); }
+	void initialise() { setup(); }
+	bool input( SDL_Event &event ) { return process_event( event ); };
+	void update( uint64_t elapsed_time ) { update_state( elapsed_time ); }
+	void draw( SDL_Renderer *window ) { draw_frame( window ); }
+
+protected:
+	virtual SetupParams get_params() = 0;
+	virtual void setup() = 0;
+	virtual bool process_event( SDL_Event &event ) = 0;
+	virtual void update_state( uint64_t elapsed_time ) = 0;
+	virtual void draw_frame( SDL_Renderer *window ) = 0;
+};
+
+template <typename T> class GameWrapper
+{
+public:
+	int run()
+	{
+		SDL_Init( SDL_INIT_EVERYTHING );
+
+		SetupParams params = aGame.make_setup();
+
+		SDL_Window *window = SDL_CreateWindow( params.title.c_str(), SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+											   params.width, params.height, params.flags );
+		SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, "linear" );
+		SDL_Renderer *renderer = SDL_CreateRenderer( window, -1, params.rendererFlags );
+
+		aGame.initialise();
+
+		uint64_t game_tick = SDL_GetTicks64();
+
+		bool quit = false;
+
+		while( !quit ) {
+
+			SDL_Event event;
+
+			while( SDL_PollEvent( &event ) ) quit = aGame.input( event );
+
+			if( quit )
+				break;
+
+			if( SDL_GetTicks64() > ( game_tick + 16 ) ) {
+
+				aGame.update( SDL_GetTicks64() - game_tick );
+
+				SDL_SetRenderDrawColor( renderer, 0, 0, 0, 255 );
+				SDL_RenderClear( renderer );
+
+				aGame.draw( renderer );
+
+				SDL_RenderPresent( renderer );
+
+				game_tick = SDL_GetTicks64();
+			}
+		}
+
+		SDL_DestroyWindow( window );
+		SDL_DestroyRenderer( renderer );
+
+		SDL_Quit();
+
+		return 0;
+	};
+
+private:
+	T aGame;
+};
+
+class BlankGame : public Game
+{
+	SetupParams get_params() override { return SetupParams( { "Blank Game", 640, 480, 0, SDL_RENDERER_ACCELERATED } ); }
+	void setup() override {}
+	bool process_event( SDL_Event &event ) override { return event.type == SDL_QUIT; }
+	void update_state( uint64_t elapsed_time ) override{};
+	void draw_frame( SDL_Renderer *window ) override{};
+};
--- src/main.cc
+++ src/main.cc
@@ -19,7 +19,215 @@
 
 #include <iostream>
 
+#include <string>
+
+#include "SDL2_gfxPrimitives.h"
+#include "sdl2wrapper.h"
+
+#define GLM_ENABLE_EXPERIMENTAL
+
+#include <glm/ext.hpp>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+int unit_size = 80;
+
+class Player
+{
+public:
+	Player( int init_x, int init_y ) : position( init_x, init_y, 0.0 ){};
+
+	void draw( SDL_Renderer *window );
+
+	void move_forward()
+	{
+		glm::mat4 trans = get_trans_matrix();
+		trans = glm::scale( trans, glm::vec3( 0.05, 0.05, 1 ) );
+		position = trans * glm::vec4( 1.0F, 0.0F, 0.0F, 1.0F );
+	}
+
+	void move_back()
+	{
+		glm::mat4 trans = get_trans_matrix();
+		trans = glm::scale( trans, glm::vec3( 0.05, 0.05, 1 ) );
+		position = trans * glm::vec4( -1.0F, 0.0F, 0.0F, 1.0F );
+	}
+
+	void turn_left() { angle -= 1.0; }
+	void turn_right() { angle += 1.0; }
+	void zoom_in() { zoom -= 0.01; }
+	void zoom_out() { zoom += 0.01; }
+
+private:
+	glm::vec3 position;
+	float angle = 0.0;
+	float zoom = 1.0;
+
+	glm::mat4 get_trans_matrix()
+	{
+
+		glm::mat4 trans = glm::mat4( 1.0F );
+		trans = glm::translate( trans, position );
+		trans = glm::rotate( trans, glm::radians( angle ), glm::vec3( 0.0F, 0.0F, 1.0F ) );
+		trans = glm::scale( trans, glm::vec3( unit_size, unit_size, 1 ) );
+
+		return trans;
+	}
+
+	// glm::vec4 transform_point( glm::vec4 point ) { return get_trans_matrix() * point; }
+};
+
+void Player::draw( SDL_Renderer *window )
+{
+	const glm::mat4 trans = get_trans_matrix();
+
+	SDL_SetRenderDrawColor( window, 1 * 255, 1 * 255, 1 * 255, 1 * 255 );
+
+	filledCircleRGBA( window, position.x, position.y, unit_size / 5, 255, 255, 0, 255 );
+
+	SDL_SetRenderDrawColor( window, 1 * 255, 0 * 255, 0 * 255, 1 * 255 );
+	const glm::vec4 world_dir = trans * glm::vec4( 1.0F, 0.0F, 0.0F, 1.0F );
+	SDL_RenderDrawLine( window, position.x, position.y, world_dir.x, world_dir.y );
+
+	SDL_SetRenderDrawColor( window, 0 * 255, 1 * 255, 0 * 255, 1 * 255 );
+	const glm::vec4 world_cam_left = trans * glm::vec4( 1.0F, zoom, 0.0F, 1.0F );
+	const glm::vec4 world_cam_right = trans * glm::vec4( 1.0F, -zoom, 0.0F, 1.0F );
+	SDL_RenderDrawLine( window, world_cam_left.x, world_cam_left.y, world_cam_right.x, world_cam_right.y );
+
+	SDL_SetRenderDrawColor( window, 0 * 255, 0 * 255, 1 * 255, 1 * 255 );
+	const glm::vec4 world_left_ray = trans * glm::vec4( 2.0F, 2 * zoom, 0.0F, 1.0F );
+	const glm::vec4 world_right_ray = trans * glm::vec4( 2.0F, 2 * -zoom, 0.0F, 1.0F );
+	SDL_RenderDrawLine( window, position.x, position.y, world_left_ray.x, world_left_ray.y );
+	SDL_RenderDrawLine( window, position.x, position.y, world_right_ray.x, world_right_ray.y );
+}
+
+class TilePaintingGame : public Game
+{
+public:
+	TilePaintingGame() : hero( 100, 100 ){};
+
+private:
+	SetupParams get_params() override;
+	void setup() override;
+	bool process_event( SDL_Event &event ) override;
+	void update_state( uint64_t elapsed_time ) override;
+	void draw_frame( SDL_Renderer *window ) override;
+
+	void draw_grid( SDL_Renderer *window );
+	void draw_level( SDL_Renderer *window );
+
+	std::string level = "1111111111"
+						"1000100001"
+						"1000100001"
+						"1000100001"
+						"1000000001"
+						"1000000001"
+						"1000000001"
+						"1000001001"
+						"1000000001"
+						"1111111111";
+
+	Player hero;
+
+	bool quit = false;
+
+	uint8_t KEY_UP = 0;
+	uint8_t KEY_DOWN = 0;
+	uint8_t KEY_LEFT = 0;
+	uint8_t KEY_RIGHT = 0;
+	uint8_t KEY_X = 0;
+	uint8_t KEY_Z = 0;
+
+	uint8_t KEY_IS_DOWN = 0x01;
+};
+
+SetupParams TilePaintingGame::get_params()
+{
+	return SetupParams( { "Blank Game", 10 * unit_size * 2, 10 * unit_size, 0, SDL_RENDERER_ACCELERATED } );
+}
+
+void TilePaintingGame::setup() {}
+
+bool TilePaintingGame::process_event( SDL_Event &event )
+{
+	if( event.type == SDL_KEYDOWN ) {
+		switch( event.key.keysym.sym ) {
+		case SDLK_UP: KEY_UP |= KEY_IS_DOWN; break;
+		case SDLK_DOWN: KEY_DOWN |= KEY_IS_DOWN; break;
+		case SDLK_LEFT: KEY_LEFT |= KEY_IS_DOWN; break;
+		case SDLK_RIGHT: KEY_RIGHT |= KEY_IS_DOWN; break;
+		case SDLK_x: KEY_X |= KEY_IS_DOWN; break;
+		case SDLK_z: KEY_Z |= KEY_IS_DOWN; break;
+		}
+	}
+
+	if( event.type == SDL_KEYUP ) {
+		switch( event.key.keysym.sym ) {
+		case SDLK_UP: KEY_UP &= ~KEY_IS_DOWN; break;
+		case SDLK_DOWN: KEY_DOWN &= ~KEY_IS_DOWN; break;
+		case SDLK_LEFT: KEY_LEFT &= ~KEY_IS_DOWN; break;
+		case SDLK_RIGHT: KEY_RIGHT &= ~KEY_IS_DOWN; break;
+		case SDLK_x: KEY_X &= ~KEY_IS_DOWN; break;
+		case SDLK_z: KEY_Z &= ~KEY_IS_DOWN; break;
+		case SDLK_ESCAPE: quit = true; break;
+		}
+	}
+
+	if( event.type == SDL_QUIT )
+		quit = true;
+
+	return quit;
+}
+
+void TilePaintingGame::update_state( uint64_t /*elapsed_time*/ )
+{
+	if( ( KEY_UP & KEY_IS_DOWN ) != 0 )
+		hero.move_forward();
+	if( ( KEY_DOWN & KEY_IS_DOWN ) != 0 )
+		hero.move_back();
+	if( ( KEY_LEFT & KEY_IS_DOWN ) != 0 )
+		hero.turn_left();
+	if( ( KEY_RIGHT & KEY_IS_DOWN ) != 0 )
+		hero.turn_right();
+	if( ( KEY_X & KEY_IS_DOWN ) != 0 )
+		hero.zoom_out();
+	if( ( KEY_Z & KEY_IS_DOWN ) != 0 )
+		hero.zoom_in();
+}
+
+void TilePaintingGame::draw_frame( SDL_Renderer *window )
+{
+	draw_grid( window );
+	draw_level( window );
+
+	hero.draw( window );
+}
+
+void TilePaintingGame::draw_grid( SDL_Renderer *window )
+{
+	SDL_SetRenderDrawColor( window, 0.3 * 255, 0.3 * 255, 0.3 * 255, 1 * 255 );
+
+	for( int line = 0; line <= 10; ++line ) {
+		SDL_RenderDrawLine( window, line * unit_size, 0, line * unit_size, 10 * unit_size );
+		SDL_RenderDrawLine( window, 0, line * unit_size, 10 * unit_size, line * unit_size );
+	}
+}
+
+void TilePaintingGame::draw_level( SDL_Renderer *window )
+{
+	SDL_SetRenderDrawColor( window, 1 * 255, 1 * 255, 1 * 255, 1 * 255 );
+
+	for( int cell = 0; cell < 100; ++cell ) {
+		if( level[cell] == '1' ) {
+			SDL_Rect const rect( { ( cell % 10 ) * unit_size, ( cell / 10 ) * unit_size, unit_size, unit_size } );
+			SDL_RenderFillRect( window, &rect );
+		}
+	}
+}
+
 int main( int /*unused*/, char ** /*unused*/ )
 {
-	std::cout << "Hello, from ray_caster!\n";
+	GameWrapper<TilePaintingGame> the_game;
+	return the_game.run();
 }
